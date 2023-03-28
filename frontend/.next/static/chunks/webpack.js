39613e1d646e6dcb1773cb84aa277f99b7c8a004
praveen-betalectic <praveen@betalectic.com>
2023-03-28 14:20:24 +0530

    Revert "staging url added"
    This reverts commit b7d879f36cd52c02bbcace0879f884587c55cee6.

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "91d2a43e78b085cab049"; }
+/******/ 		__webpack_require__.h = function() { return "0599709ddf85873000fb"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- frontend/next.config.js
+++ frontend/next.config.js
@@ -4,7 +4,7 @@
 const nextConfig = {
   images: {
     loader: "default",
-    domains: ["api.staging.finezzy.com"],
+    domains: ["localhost"],
   },
 }
 
